d2aabf3ad147ad8bb3c74a1771a5a2b7be432acc
Daniel Garay <garay.daniel@protonmail.com>
2021-11-14 19:46:48 -0500

    swap js tests

--- src/Swap.js
+++ src/Swap.js
@@ -4,7 +4,10 @@ import { PublicKey, Keypair, SystemProgram, Transaction } from '@solana/web3.js'
 import React, { useCallback } from 'react';
 import { Provider, Program} from '@project-serum/anchor';
 
-const MOONRACE_PROGRAM_ID = '6dsJRgf4Kdq6jE7Q5cgn2ow4KkTmRqukw9DDrYP4uvij';
+export const MOONRACE_PROGRAM_ID = '6dsJRgf4Kdq6jE7Q5cgn2ow4KkTmRqukw9DDrYP4uvij';
+export const HEDGE_PROGRAM_ID = 'devhGU5ma3S4CwsxX3ovDV5zY7K2bx7ZDQLhxYxVn11'
+// export const HEDGE_PROGRAM_ID = 'HEDGEau7kb5L9ChcchUC19zSYbgGt3mVCpaTK6SMD8P4'
+
 
 
 export const Swap = () => {
@@ -17,7 +20,7 @@ export const Swap = () => {
         if (!publicKey) throw new WalletNotConnectedError();
 
         const provider = new Provider(connection, wallet)
-        const program = await Program.at(new PublicKey(MOONRACE_PROGRAM_ID), provider)
+        const program = await Program.at(new PublicKey(HEDGE_PROGRAM_ID), provider)
         console.log(program);
 
         // const transaction = new Transaction().add(
